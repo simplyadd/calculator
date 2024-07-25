872b4c64fa344866e7fd0ca0774ea54a1bf5e30e
simplyadd <69213787+simplyadd@users.noreply.github.com>
2024-07-24 23:39:30 -0500

    Move snarky comments to a new function for readability

--- main.js
+++ main.js
@@ -34,43 +34,16 @@ btns.forEach( (button) => {
 				two_nums.push(temp_num);	// Save number
 			}
 
+
 			if (two_nums.length === 2) {
 				// Get result using previously saved operator
 				temp_num = operate(parseInt(two_nums[0]), operator, parseInt(two_nums[1]));
-				if (isNaN(temp_num) && two_nums[1] == 0) {
-					window.alert("Is your divisor the number of friends you haveɀ̣");
-					window.alert("...or maybe your current working brain cells?");
-					window.alert("Go to sleep, take a break, hydrate or something.");
-				}
-
 				two_nums.shift();
 				two_nums[0] = `${temp_num}`;
+
 			} else if (operator == "=") {
 				eq_count += 1;
-				switch (eq_count) {
-					case 1:
-						temp_num = `${two_nums[0]} = ${two_nums[0]}`
-						break;
-					case 2:
-						window.alert("Try clicking equals again.");
-						break;
-					case 3:
-						window.alert("Aren't you obedient?");
-						break;
-					case 4:
-						window.alert("IT'S NOT GOING TO CHANGE‽");
-						break;
-					case 5:
-						window.alert("ya know what");
-						temp_num = 80085;
-						break;
-					default:
-						temp_num = 800845;
-						eq_count = 0;
-						break;
-				}
-
-
+				snark_attack(eq_count);
 			}
 
 			flag_new = true;
@@ -81,6 +54,40 @@ btns.forEach( (button) => {
 		screen.textContent = temp_num;
 	});
 });
+
+/* 0 is snark comments for dividing by 0.
+ * Any other number is an input to the eq_count
+ * 	switch case for repeatedly clicking '=' button.
+ */
+function snark_attack(NaN_or_eq) {
+	switch (eq_count) {
+		case 0:
+			window.alert("Is your divisor the number of friends you haveɀ̣");
+			window.alert("...or maybe your current working brain cells?");
+			window.alert("Go to sleep, take a break, hydrate or something.");
+			break;
+		case 1:
+			temp_num = `${two_nums[0]} = ${two_nums[0]}`
+			break;
+		case 2:
+			window.alert("Try clicking equals again.");
+			break;
+		case 3:
+			window.alert("Aren't you obedient?");
+			break;
+		case 4:
+			window.alert("IT'S NOT GOING TO CHANGE‽");
+			break;
+		case 5:
+			window.alert("ya know what");
+			temp_num = 80085;
+			break;
+		default:
+			temp_num = 800845;
+			eq_count = 0;
+			break;
+	}
+}
 
 function operate(a, operator, b) {
 	switch (operator) {
@@ -110,7 +117,7 @@ const multiply = function (a, b) {
 };
 
 const divide = function (a, b) {
-	if (b === 0) return "UNDEFINED";
+	if (b === 0) snark_attack(0);
 	return a / b;
 };
 
